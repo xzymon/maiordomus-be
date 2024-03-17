e7a3bf8001a55b0b6900e6af96cd6e9e08e12cb2
Szymon Ignaciuk <xzymon@gmail.com>
2024-03-17 17:38:02 +0100

    Starter

---
+++ src/main/java/com/xzymon/maiordomus/MaiordomusApplication.java
@@ -0,0 +1,13 @@
+package com.xzymon.maiordomus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaiordomusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaiordomusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xzymon/maiordomus/MaiordomusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xzymon.maiordomus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaiordomusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
