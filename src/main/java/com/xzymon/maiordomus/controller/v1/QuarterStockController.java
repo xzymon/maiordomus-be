2209bd0665ef90b49a245bb44c3ddb0614ef5865
xzymon <xzymon@gmail.com>
2025-01-18 21:55:33 +0100

    Added OpenAPI with docs. Closes #16

--- src/main/java/com/xzymon/maiordomus/controller/v1/QuarterStockController.java
+++ src/main/java/com/xzymon/maiordomus/controller/v1/QuarterStockController.java
@@ -4,6 +4,8 @@ import com.xzymon.maiordomus.dto.StockCandleDTO;
 import com.xzymon.maiordomus.mapper.DefaultMapper;
 import com.xzymon.maiordomus.model.db.StockCandle;
 import com.xzymon.maiordomus.service.stock.QuarterStockService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -17,6 +19,7 @@ import java.util.List;
 
 @Controller
 @RequestMapping(QuarterStockController.BASE_URL)
+@Tag(name = "Quarter stock controller")
 public class QuarterStockController {
 	public static final String BASE_URL = "/api/v1/qstock";
 	private static final Logger LOGGER = LoggerFactory.getLogger(QuarterStockController.class);
@@ -29,10 +32,21 @@ public class QuarterStockController {
 
 	@CrossOrigin(origins = "*")
 	@GetMapping("/all")
+	@Operation(summary = "Pobierz wszystkie informacje o akcjach", description = "Zapytanie zwraca wszystkie akcje ze szczegółami w formie DTO.")
 	public ResponseEntity<List<StockCandleDTO>> getAll() {
 		LOGGER.info("Performing getAll");
 		List<StockCandle> fromDB = quarterStockService.getAll();
 		List<StockCandleDTO> dtos = fromDB.stream().map(DefaultMapper.INSTANCE::toStockCandleDto).toList();
 		return new ResponseEntity<>(dtos, HttpStatus.OK);
 	}
+
+	/*
+	@CrossOrigin(origins = "*")
+	@GetMapping("/all")
+	public ResponseEntity<List<StockCandleDTO>> getAll() {
+		LOGGER.info("Performing getAll");
+		List<StockCandle> fromDB = quarterStockService.getAll();
+		List<StockCandleDTO> dtos = fromDB.stream().map(DefaultMapper.INSTANCE::toStockCandleDto).toList();
+		return new ResponseEntity<>(dtos, HttpStatus.OK);
+	}*/
 }
